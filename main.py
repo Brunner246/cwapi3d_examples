b00826bf1f7ce458ef87444f9e5fd0e86a1d9087
Michael Brunner <brunner@cadwork.swiss>
2024-01-23 17:04:33 +0100

    - initial commit

--- main.py
+++ main.py
@@ -7,34 +7,36 @@ import geometry_controller as gc
 
 class Room:
     def __init__(self, element_id: int):
-        self.__element_id = element_id
+        self.element_id = element_id
         self.__foot_print_vertices = list()
+        self.__foot_print_facet: List[cadwork.point_3d] = list()
 
     def get_foot_print_vertices(self):
         if not self.__foot_print_vertices:
-            self.__foot_print_vertices = gc.get_element_vertices(self.element_id)
-            sorted(self.__foot_print_vertices, key=lambda vertex: vertex.z)
-            threshold = self.__foot_print_vertices[0].z
-            self.__foot_print_vertices = [vertex for vertex in self.__foot_print_vertices
-                                          if (abs(vertex.z) - threshold) < 1e-4]
-
+            vertices = gc.get_element_vertices(self.element_id)
+            lowest_vertex = min(vertices, key=lambda vertex: vertex.z)
+            threshold = lowest_vertex.z
+            self.__foot_print_vertices = [vertex for vertex in vertices
+                                          if abs(vertex.z - threshold) < 1e-4]
         return self.__foot_print_vertices
 
-    @property
-    def element_id(self):
-        return self.__element_id
+    def get_foot_print_facet(self):
+        facets: cadwork.facet_list = gc.get_element_facets(self.element_id)
+        vertices: List[cadwork.vertex_list] = list()
+        for facet in range(facets.count()):
+            vertices.append(facets.get_external_polygon(facet))
 
-    @element_id.setter
-    def element_id(self, element_id):
-        self.__element_id = element_id
+        self.__foot_print_facet = min(vertices, key=lambda vertex: vertex.at(0).z)
+
+        return [self.__foot_print_facet.at(i) for i in range(self.__foot_print_facet.count())]
 
 
-room_objects: List[int] = ec.get_user_element_ids()
+if __name__ == '__main__':
 
-rooms: List[Room] = list()
-for room_object in room_objects:
-    rooms.append(Room(room_object))
+    room_objects: List[int] = ec.get_user_element_ids()
 
-for room in rooms:
-    print(room.get_foot_print_vertices())
-    [ec.create_node(cadwork.point_3d(vertex.x, vertex.y, vertex.z)) for vertex in room.get_foot_print_vertices()]
+    rooms: List[Room] = [Room(room_object) for room_object in room_objects]
+
+    for room in rooms:
+        [ec.create_node(cadwork.point_3d(vertex.x, vertex.y, vertex.z)) for vertex in room.get_foot_print_vertices()]
+        ec.create_surface(room.get_foot_print_facet())
