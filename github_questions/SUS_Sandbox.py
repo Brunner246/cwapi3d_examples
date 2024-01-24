b86e504686f18581cefdb0419a64d23c18961864
Michael Brunner <brunner@cadwork.swiss>
2024-01-24 15:07:11 +0100

    - import modules

--- github_questions/SUS_Sandbox.py
+++ github_questions/SUS_Sandbox.py
@@ -1,4 +1,3 @@
-from typing import List
 import sys
 
 print("sandbox start")
@@ -14,3 +13,22 @@ from Sandbox.TestObject import Display
 Display()
 
 print("sandbox end")
+
+
+# or
+
+# import sys
+#
+# print("sandbox start")
+#
+# import utility_controller as uc
+#
+# plugin_path = uc.get_plugin_path()
+#
+# sys.path.append(plugin_path)
+#
+# import Sandbox.TestObject
+#
+# Sandbox.TestObject.Display()
+#
+# print("sandbox end")
